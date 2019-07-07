7fa5914a63c5702d949b80ae94bc3a60584e56ef
Jonas <49783797+n01deas@users.noreply.github.com>
2019-07-07 16:18:50 +0200

    Update RollTheDice.java

--- Mensch-rgeredichnicht-master/Ludo/src/game/RollTheDice.java
+++ Mensch-rgeredichnicht-master/Ludo/src/game/RollTheDice.java
@@ -5,22 +5,24 @@ import java.util.Random;
 import javafx.scene.image.Image;
 
 /**
- * Diese Klasse ist für das Würfel verantwortlich
+ * Diese Klasse ist fÃ¼r das WÃ¼rfel verantwortlich
  * 
  * @author Lukas, Kim, Isabelle, Jonas
  * @version 1.0
  */
 public class RollTheDice {
 
-	// Variable enthält die gewürfelte Zahl
+	// Variable enthÃ¤lt die gewÃ¼rfelte Zahl
 	public int cubeNumber;
 	
 	/*
-	 * Diese Methode würfelt eine Zahl zwischen 1 und 6
+	 * Diese Methode wÃ¼rfelt eine Zahl zwischen 1 und 6
 	 */
-	public void rollTheDice() {
+	public int rollTheDice() {
 		Random random = new Random();
-		cubeNumber = random.nextInt(6)+1;
+		int cubeNumber = random.nextInt(6)+1;
+		
+		return cubeNumber;
 		
 		/**if(cubeNumber == 1) {
 			Image im = new Image("/GameBoardFourPlayers.Pictures/Dice1.png", true);
